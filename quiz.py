034351d2950c422e65b013a640693a3ec1cda389
naturb <63774156+naturb@users.noreply.github.com>
2020-05-05 11:18:01 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,55 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# niezbędne importy
 from flask import Flask
 from flask import render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
-# konfiguracja aplikacji
 app.config.update(dict(
-    # nieznany nikomu sekret
-    SECRET_KEY='bradzosekretnawartosc',
+    SECRET_KEY='secret',
 ))
 
-# lista pytań
 PYTANIA = [
     {
-        'pytanie': u'Stolica Hiszpani, to:',# pytanie
-        'odpowiedzi': [u'Madryt', u'Warszawa', u'Barcelona'], # możliwe odpowiedzi
-        'odpok': u'Madryt', # poprawna odpowiedź
+        'pytanie': u'Stolica Polski to:',
+        'odpowiedzi': [u'Kraków', u'Warszawa', u'Gniezno'],
+        'odpok': u'Warszawa',
     },
     {
-        'pytanie': u'Objętość sześcianu o boku 6 cm, wynosi:',
-        'odpowiedzi': [u'36', u'216', u'18'],
-        'odpok': u'216',
+        'pytanie': u'Wynik dzielenia to:',
+        'odpowiedzi': [u'iloraz', u'iloczyn', u'suma'],
+        'odpok': u'iloraz',
     },
     {
-        'pytanie': u'Symbol pierwiastka Helu, to:',
-        'odpowiedzi': [u'Fe', u'H', u'He'],
-        'odpok': u'He',
+        'pytanie': u'Wynik działania 2*2+2 to:',
+        'odpowiedzi': [u'4', u'8', u'6'],
+        'odpok': u'6',
     }
 ]
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # jeżeli żądanie jest typu POST, tzn. że przesłano odpowiedzi do sprawdzenia
     if request.method == 'POST':
-        punkty = 0 # liczba poprawnych odpowiedzi
-        odpowiedzi = request.form # zapamiętujemy przesłane odpowiedzi
-        # sprawdzamy odpowiedzi
+        punkty = 0
+        odpowiedzi = request.form 
         for pnr, odp_u in odpowiedzi.items():
-            # porównujemy przesłaną odpowiedź z poprawną
             if odp_u == PYTANIA[int(pnr)]['odpok']:
-                punkty += 1 # zwiększamy wynik
-        # przygotowujemy informację o wyniku
+                punkty += 1
         flash(u'Liczba poprawnych odpowiedzi, to: {0}'.format(punkty))
-        # przekierowujemy na stronę główną
         return redirect(url_for('index'))
 
-    # jeżeli zadanie jest typu GET, to renderujemy szablon index.html,
-    # do którego przekazujemy tablicę pytań w zmiennej "pytania"
     return render_template('index.html', pytania=PYTANIA)
 
 if __name__ == '__main__':
